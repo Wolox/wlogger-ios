98ea059e8d8c1eb20dcd441d72b395256d91cfa1
Pablo Giorgi <pablo.giorgi@wolox.com.ar>
2016-01-29 15:44:13 -0300

    WeekLogsTableViewController as final

--- WLogger/WeekLogs/WeekLogsTableViewController.swift
+++ WLogger/WeekLogs/WeekLogsTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeekLogsTableViewController: UITableViewController {
+final class WeekLogsTableViewController: UITableViewController {
 
     var viewModel: WeekLogsTableViewModel?
 }
